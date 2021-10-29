04502dd82c9f5311cbd94fa5a253a49372628ca7
Rahul Bhargave <rahul.bhargave@synfioo.com>
2021-10-29 15:23:49 +0200

    cors error resolved between two s3 buckets

--- src/config/http.apis.ts
+++ src/config/http.apis.ts
@@ -1,10 +1,10 @@
 import http from './http.common';
 
 const get = () => {
-    return http.get(`/resumeData.json`);
+    return http.get(`${process.env.REACT_APP_BASE_URL}/resumeData.json`);
 };
 
-const getRepos = () => {
+const getRepos = () =>{
     return http.get(`https://api.github.com/users/${process.env.REACT_APP_GIT_USER}/repos?per_page=5`);
 }
 
--- src/config/http.common.ts
+++ src/config/http.common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `${process.env.REACT_APP_BASE_URL}`,
+  baseURL: "/resume.static.data",
   headers: {
     "Content-type": "application/json"
   }
